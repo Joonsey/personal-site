4c78f19372a77774ddc4cca08b4810a964ac9474
Johannes <jonseyplayz@gmail.com>
2022-05-31 06:24:17 +0200

    fixing static files serving

--- website/website/settings.py
+++ website/website/settings.py
@@ -131,7 +131,7 @@ if os.system == "linux":
     STATIC_ROOT = '/home/jonsey/web-server/static/'
 else:
     STATIC_ROOT = os.path.join(os.getcwd(), 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
